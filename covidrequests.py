784ec642e5b55d78b1823982bd01855b51430edf
Joe Holden <joseph.holden@rocketmail.com>
2020-10-05 16:46:42 +0100

    Added functionality for the user to choose the country they want to see. Also added death functionality.

--- covidrequests.py
+++ covidrequests.py
@@ -1,15 +1,31 @@
 import requests
 
 
-class CountryAllStatus:
+class ByCountryTotal:
 
-    def __init__(self, url, date1, date2):
-        self.url = url
+    def __init__(self, country, date1, date2):
+        self.country = country
         self.date1 = date1
         self.date2 = date2
 
     def request(self):
-        url = self.url + "?from=" + str(self.date1) + "&to=" + str(self.date2)
+        url = "https://api.covid19api.com/total/country/" + self.country + "/status/confirmed" + "?from=" + \
+              str(self.date1) + "&to=" + str(self.date2)
+        response = requests.get(url).json()
+
+        return response
+
+
+class CountryAllStatus:
+
+    def __init__(self, country, date1, date2):
+        self.country = country
+        self.date1 = date1
+        self.date2 = date2
+
+    def request(self):
+        url = "https://api.covid19api.com/country/" + self.country + "?from=" + str(self.date1) + \
+              "&to=" + str(self.date2)
         response = requests.get(url).json()
 
         return response
--- covidtracker.py
+++ covidtracker.py
@@ -1,27 +1,76 @@
 import datetime
 import re
+import pycountry
 from covidrequests import *
 
 
-def countryallstatus():
+def welcomescreen():
+    print("Welcome to the Covid-19 Tracker App")
+
+    ans = input("What would you like to see?\n1) Confirmed Cases\t*N/A\n2) Recovered\t*N/A\n3) Deaths\n"
+                "4) Days since first confirmed case\t*N/A\n5) Today's Confirmed Cases\n")
+    print("Please enter the country you would like to see (Using their 3 letter country codes)")
+    country = input("eg. IRL = Ireland, GBR = United Kingdom, WOR = World\n").upper()
+    if country != "WOR":
+        c = pycountry.countries.get(alpha_3=country).name
+
+    for i in range(len(c)):
+        if c[i] == ' ':
+            country = c.replace(c[i], '-')
+
+    if ans == "1":
+        print("Not available yet! Sorry")
+    elif ans == "2":
+        print("Not available yet! Sorry")
+    elif ans == "3":
+        deaths(country)
+    elif ans == "4":
+        print("Not available yet! Sorry")
+    elif ans == "5":
+        countrytotal(c)
+
+
+def countrytotal(country):
     today = datetime.datetime.now()
     datey = datetime.datetime(today.year, today.month, today.day-1)
     dateb = datetime.datetime(today.year, today.month, today.day-2, 23, 59, 59)
     date2daysago = datetime.datetime(today.year, today.month, today.day - 3, 23, 59, 59)
 
-    temp = CountryAllStatus("https://api.covid19api.com/country/ireland", dateb, datey)
-    r = CountryAllStatus.request(temp)
+    temp = ByCountryTotal(country, dateb, datey)
+    r = ByCountryTotal.request(temp)
     response = urlcleanup(r)
 
-    casesyest = response["Confirmed"]
+    casesyest = response["Cases"]
 
-    temp = CountryAllStatus("https://api.covid19api.com/country/ireland", date2daysago, dateb)
-    r = CountryAllStatus.request(temp)
+    temp = ByCountryTotal(country, date2daysago, dateb)
+    r = ByCountryTotal.request(temp)
     response = urlcleanup(r)
 
-    cases2days = response["Confirmed"]
+    cases2days = response["Cases"]
 
     print("Cases Confirmed Yesterday:\t", int(casesyest) - int(cases2days))
+    print("This number includes all cases for all the different territories of this country")
+
+
+def deaths(country):
+    today = datetime.datetime.now()
+    datey = datetime.datetime(today.year, today.month, today.day - 1)
+    dateb = datetime.datetime(today.year, today.month, today.day - 2, 23, 59, 59)
+    date2daysago = datetime.datetime(today.year, today.month, today.day - 3, 23, 59, 59)
+
+    temp = CountryAllStatus(country, dateb, datey)
+    r = CountryAllStatus.request(temp)
+    response = urlcleanup(r)
+
+    casesyest = response["Deaths"]
+
+    temp = CountryAllStatus(country, date2daysago, dateb)
+    r = CountryAllStatus.request(temp)
+    response = urlcleanup(r)
+
+    cases2days = response["Deaths"]
+
+    print("Deaths Confirmed Yesterday:\t", int(casesyest) - int(cases2days))
 
 
 def urlcleanup(r):
@@ -38,4 +87,4 @@ def urlcleanup(r):
     return mydict
 
 
-countryallstatus()
+welcomescreen()
