e946b869d5521f02ab1f3669edf269c44d127a6c
Joe Holden <joseph.holden@rocketmail.com>
2020-10-06 14:24:20 +0100

    Added new fucntions: totalnumbers, newcases, newdeaths, dayssincefirst Also started working on specificdates

--- covidtracker.py
+++ covidtracker.py
@@ -8,38 +8,53 @@ def welcomescreen():
     print("Welcome to the Covid-19 Tracker App")
 
     ans = input("What would you like to see?\n"
-                "1) Confirmed Cases\t*N/A\t\t\t2) Recovered\t*N/A\n"
-                "3) Deaths\t\t\t\t\t\t\t4) Days since first confirmed case\t*N/A\n"
-                "5) Today's Confirmed Cases\t\t\t6) World Cases\n"
-                "7) Cases Betweem Specific Dates\n")
+                "1) Total Numbers for country (Confirmed, Recoveries, Deaths & Active cases)\n"
+                "2) Deaths\t\t\t\t\t\t\t"
+                "3) Days since first confirmed case\t*N/A\n"
+                "4) Today's Confirmed Cases\t\t\t"
+                "5) World Cases\n"
+                "6) Cases Betweem Specific Dates\t*N/A\n")
     
-    if ans != "6":
+    if ans != "5":
         print("Please enter the country you would like to see (Using their 3 letter country codes)")
         country = input("eg. IRL = Ireland, GBR = United Kingdom, USA = USA\n").upper()
-        if country != "WOR":
-            c = pycountry.countries.get(alpha_3=country).name
+        c = pycountry.countries.get(alpha_3=country).name
 
         for i in range(len(c)):
             if c[i] == ' ':
                 c = c.replace(c[i], '-')
 
         if ans == "1":
-            print("Not available yet! Sorry")
+            totalnumbers(c)
         elif ans == "2":
-            print("Not available yet! Sorry")
+            newdeaths(c)
         elif ans == "3":
-            deaths(country)
+            dayssincefirst(c)
         elif ans == "4":
-            print("Not available yet! Sorry")
-        elif ans == "5":
-            countrytotal(c)
-        elif ans == "7":
-            specificdates(c)
+            newcases(c)
+        elif ans == "6":
+            print("Not available sorry")
+            # specificdates(c)
     else:
         worldcases()
 
 
-def countrytotal(country):
+def totalnumbers(country):
+    today = datetime.datetime.now()
+    datey = datetime.datetime(today.year, today.month, today.day - 1)
+
+    temp = CountryAllStatus(country, datey, today)
+    r = CountryAllStatus.request(temp)
+    response = urlcleanup(r)
+
+    print("Total numbers for " + country)
+    print("Total Cases:\t\t" + response["Confirmed"] +
+          "\nTotal Deaths:\t\t" + response["Deaths"] +
+          "\nTotal Recoveries:\t" + response["Recovered"] +
+          "\nTotal Active Cases:\t" + response["Active"])
+
+
+def newcases(country):
     today = datetime.datetime.now()
     datey = datetime.datetime(today.year, today.month, today.day-1)
     dateb = datetime.datetime(today.year, today.month, today.day-2, 23, 59, 59)
@@ -61,7 +76,7 @@ def countrytotal(country):
     print("This number includes all cases for all the different territories of this country")
 
 
-def deaths(country):
+def newdeaths(country):
     today = datetime.datetime.now()
     datey = datetime.datetime(today.year, today.month, today.day - 1)
     dateb = datetime.datetime(today.year, today.month, today.day - 2, 23, 59, 59)
@@ -82,12 +97,22 @@ def deaths(country):
     print("Deaths Confirmed Yesterday:\t", int(casesyest) - int(cases2days))
 
 
+def dayssincefirst(country):
+    temp = DayOne(country)
+    r = DayOne.request(temp)
+    dets = r[0]
+    date = dets["Date"]
+
+    date = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ").date()
+    print("The first reported case for " + country + " was on: " + str(date.day) +
+          "/" + str(date.month) + "/" + str(date.year))
+
+
 def worldcases():
     today = datetime.datetime.now()
     datey = datetime.datetime(today.year, today.month, today.day - 1)
-    dateb = datetime.datetime(today.year, today.month, today.day - 2, 23, 59, 59)
 
-    temp = WorldCases(dateb, datey)
+    temp = WorldCases(datey, today)
     r = WorldCases.request(temp)
     response = urlcleanup(r)
 
@@ -98,7 +123,7 @@ def worldcases():
     print("New Cases:\t" + cases + "\nNew Deaths:\t" + deaths + "\nNew Recoveries:\t" + recovered)
 
 
-def specificdates(country):
+"""def specificdates(country):
     date1 = input("Please enter a start date in the form dd/mm/yyyy:\n")
     date2 = input("Please enter an end date in the form dd/mm/yyyy:\n")
     date1 = date1.split("/")
@@ -108,9 +133,7 @@ def specificdates(country):
     date2 = datetime.datetime(int(date2[2]), int(date2[1]), int(date2[0]))
 
     temp = CountryAllStatus(country, date1, date2)
-    r = CountryAllStatus.request(temp)
-
-
+    r = CountryAllStatus.request(temp)"""
 
 
 def urlcleanup(r):
